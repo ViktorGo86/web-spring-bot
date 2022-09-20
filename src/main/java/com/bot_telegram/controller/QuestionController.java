84f014f307ba2680fe970c47be5bb95c170a7dfc
Viktor Gnidenko <gvv.86@mail.ru>
2022-09-21 01:36:35 +0300

    post

--- src/main/java/com/bot_telegram/controller/QuestionController.java
+++ src/main/java/com/bot_telegram/controller/QuestionController.java
@@ -37,18 +37,13 @@ public class QuestionController {
         return "question_form";
     }
 
-    /*@PostMapping("/questions/save")
-    public String saveQuestion(Question question) {
-        repo.save(question);
-
-        return "redirect:/questions";
-    }*/
-	@GetMapping("/questions/save")
+    @PostMapping("/questions/save")
     public String saveQuestion(Question question) {
         repo.save(question);
 
         return "redirect:/questions";
     }
+
 
     @GetMapping("/questions/edit/{id}")
     public String showEditQuestionForm(@PathVariable("id") Integer id, Model model) {
